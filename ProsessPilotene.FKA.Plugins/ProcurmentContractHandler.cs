f5cc2b24393a535515ba7d54fc3685dcf8ed4cdb
youneshajji <younes.hajji@prosesspilotene.no>
2017-04-11 15:04:38 +0200

    ny plugin for contract team

---
+++ ProsessPilotene.FKA.Plugins/ProcurmentContractHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace ProsessPilotene.FKA.Plugins
+{
+    public class ProcurmentContractHandler
+    {
+        public void HandleTeamRoles(Entity postEntity, IOrganizationService service)
+        {
+            try
+            {
+                var procOpportunity = postEntity.GetAttributeValue<EntityReference>("pp_procurementprocessid");
+
+                if (procOpportunity == null)
+                    return;
+
+                var contractTeams = GetContractTeams(procOpportunity, service);
+                CreatContractResponsibles(postEntity, contractTeams, service);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+      
+        private List<Entity> GetContractTeams(EntityReference procOpportunity, IOrganizationService service)
+        {
+            try
+            {
+                var query = new QueryExpression("pp_opportunityteam");
+                query.ColumnSet = new ColumnSet("pp_userid", "pp_role", "pp_categoryteam");
+                query.Criteria.AddCondition("pp_opportunityid", ConditionOperator.Equal, procOpportunity.Id);
+                var results = service.RetrieveMultiple(query);
+
+                return results.Entities.ToList();
+
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+
+        private void CreatContractResponsibles(Entity postEntity, List<Entity> contractTeams, IOrganizationService service)
+        {
+            try
+            {
+                foreach (var contractTeam in contractTeams)
+                {
+                    var entity = new Entity("pp_contractteam");
+                    entity["pp_contractid"] = postEntity.ToEntityReference();
+                    entity["pp_userid"] = contractTeam.GetAttributeValue<EntityReference>("pp_userid");
+                    entity["pp_role"] = contractTeam.GetAttributeValue<OptionSetValue>("pp_role");
+                    entity["pp_categoryteam"] = contractTeam.GetAttributeValue<bool>("pp_categoryteam");
+
+                    service.Create(entity);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ProsessPilotene.FKA.Plugins/pp_procurementcontract.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace ProsessPilotene.FKA.Plugins
+{
+    public class pp_procurementcontract : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService =
+               (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") &&
+        context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+                Entity postEntity = context.PostEntityImages["PostImage"];
+
+                // Verify that the target entity represents an account.
+                // If not, this plug-in was not registered correctly.
+                if (entity.LogicalName != "pp_procurementcontract")
+                    return;
+
+                // Obtain the organization service reference.
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+
+                try
+                {
+                    if (context.MessageName.ToLower() == "create")
+                        new ProcurmentContractHandler().HandleTeamRoles(postEntity, service);
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in the plug-in: pp_procurementcontract. " + ex.Message, ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("Plugin error: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
